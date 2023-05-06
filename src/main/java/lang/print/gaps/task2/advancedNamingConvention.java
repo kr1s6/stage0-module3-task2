6d0f67521872d8aedc1f3bf6e96e116968726ce3
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-06 17:36:36 +0200

    Finished.

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,15 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
-
-    private int AGE;
+    private static final int ADULT_AGE = 18;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
+        System.out.println(phoneNumber + age);
     }
 
-    void callByNumber(int Number) {
-        //some code
+    void callByNumber(int number) {
+        System.out.println(number + ADULT_AGE);
     }
 }
